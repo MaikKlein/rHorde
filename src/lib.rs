a9c11a893f3865ec7bcf7bb7b48d0cc0e71ffaa3
maik <maikklein@googlemail.com>
2013-10-30 18:00:30 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,27 @@
 #[link(name = "horde",
 vers = "0.1",
 uuid = "6199FAD3-6D03-4E29-87E7-7DC1B1B65C2C",
-author = "Brendan Zabarauskas",
-url = "https://github.com/bjz/glfw3-rs")];
+author = "Maik Klein",
+url = "https://github.com/MaikKlein/rHorde/")];
 
-#[comment = "Bindings and wrapper functions for glfw3."];
+#[comment = "Horde3D bindings for Rust."];
 #[crate_type = "lib"];
 #[feature(globs)];
 #[feature(macro_rules)];
 pub mod ffi;
 
-
 #[fixed_stack_segment]
 fn h3d_init()->bool{
   unsafe { ffi::h3dInit()}
+}
+
+#[fixed_stack_segment]
+fn h3d_clear(){
+  unsafe{ffi::h3dClear()};
+}
+
+#[fixed_stack_segment]
+fn h3d_release(){
+  unsafe{ ffi::h3dRelease();}
 }
 
